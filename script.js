cb86fe65f77abf9fb91a140720d1198c69e326ed
camis-sntox <camila.neris.santos@escola.pr.gov.br>
2025-04-29 12:23:17 -0300

    alterando menu

--- script.js
+++ script.js
@@ -219,21 +219,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+        const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+        const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    let tamanhoAtualFonte = 1;
+        let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        aumentaFonteBotao.addEventListener('click', function () {
+            tamanhoAtualFonte += 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
 
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-})
+        diminuiFonteBotao.addEventListener('click', function () {
+            tamanhoAtualFonte -= 0.1;
+            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        })
+});
